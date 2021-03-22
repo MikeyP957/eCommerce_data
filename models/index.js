f2c3edcab1ce3454b8199f8494b22157e942cd48
Mikey P <peraram2@gmail.com>
2021-03-22 15:53:29 -0700

    index.js finished

--- models/index.js
+++ models/index.js
@@ -5,12 +5,40 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE',
+})
 // Categories have many Products
+Category.hasMany(
+    Product, {
+        forgeignKey: 'category_id',
+        onDelete: 'CASCADE',
+    }
+)
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(
+    Tag, {
+        through: {
+            model: ProductTag,
+            unique: false,
+        },
+        //as : ''
+    }
+);
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(
+    Product, {
+        through: {
+            ProductTag,
+            unique: false,
+        }
+     //alias
+        //as: ''
+    }
+)
+
 
 module.exports = {
   Product,
