c1eb9b80b19a0150402a878d58a7935f0b46e47d
Mikey P <peraram2@gmail.com>
2021-03-22 14:38:24 -0700

    productTag model

---
+++ models/ProductTag.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+ {    
+    id: {
+      type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'Product',
+            key: 'id',
+            unique: false,
+        },
+        tag_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Tag',
+                key: 'id',
+                unique: false,
+            }
+        }
+    },
+        
+ },
+  {
+    sequelize,
+    timestamps: false,
+    underscored: true,
+    modelName: 'ProductTag'
+  }
+);
+
+module.exports = ProductTag;
