751fd3a0df488014bbcc01e7b49af513683af918
Mikey P <peraram2@gmail.com>
2021-03-25 18:13:35 -0700

    refractor, err --> .json(...Data)

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -60,7 +60,7 @@ router.put('/:id', async (req, res) => {
             res.status(404).json({message: 'There is no category with this id.'})
         }
 
-        res.status(200).json(res)
+        res.status(200).json(categoryData)
     } catch (err) {
         res.status(500).json(err);
     }
